82a6eabcacbddf996ceb08253f861e5d08acefe3
Bruna Suelen <subr.garcia@gmail.com>
2022-05-21 19:05:21 -0300

    feat: Corrige inicialização de arraylist

--- src/uteis/Rotina.java
+++ src/uteis/Rotina.java
@@ -5,8 +5,9 @@ import classes.Filme;
 
 
 public class Rotina {
+	
 	public static ArrayList<Filme> gerarFilmes() {
-		ArrayList<Filme> filmes = null;
+		ArrayList<Filme> filmes = new ArrayList<Filme>();
 		filmes.add(new Filme("Homem Aranha", "Ação", "02h", 10.00, 12));
 		filmes.add(new Filme("Homem Aranha", "Ação", "02h", 10.00, 12));
 		filmes.add(new Filme("Homem Aranha", "Ação", "02h", 10.00, 12));
