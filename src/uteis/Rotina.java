d7ed04f071435fbc94c7fd280058bd05e18d4da9
Bruna Suelen <subr.garcia@gmail.com>
2022-05-21 18:43:29 -0300

    feat: Adiciona primeira rotina
    Cria os 8 filmes iniciais

---
+++ src/uteis/Rotina.java
@@ -0,0 +1,20 @@
+package uteis;
+
+import java.util.ArrayList;
+import classes.Filme;
+
+
+public class Rotina {
+	public static ArrayList<Filme> gerarFilmes() {
+		ArrayList<Filme> filmes = null;
+		filmes.add(new Filme("Homem Aranha", "Ação", "02h", 10.00, 12));
+		filmes.add(new Filme("Homem Aranha", "Ação", "02h", 10.00, 12));
+		filmes.add(new Filme("Homem Aranha", "Ação", "02h", 10.00, 12));
+		filmes.add(new Filme("Homem Aranha", "Ação", "02h", 10.00, 12));
+		filmes.add(new Filme("Homem Aranha", "Ação", "02h", 10.00, 12));
+		filmes.add(new Filme("Homem Aranha", "Ação", "02h", 10.00, 12));
+		filmes.add(new Filme("Homem Aranha", "Ação", "02h", 10.00, 12));
+		filmes.add(new Filme("Homem Aranha", "Ação", "02h", 10.00, 12));		
+		return filmes;
+	}
+}
