508beb78a13aa673811c49414079aca065673e49
Bruna Suelen <subr.garcia@gmail.com>
2022-05-22 01:09:18 -0300

    feat: Adiciona fluxo do metodo registrar filme

--- src/Principal.java
+++ src/Principal.java
@@ -1,12 +1,11 @@
 import classes.Filme;
 import classes.Locadora;
-import java.io.IOException;
 import uteis.*;
 
 
 public class Principal {
 
-	public static void main(String[] args) throws InterruptedException, IOException {
+	public static void main(String[] args) {
 		String opcaoMenu;
 		Locadora locadora = new Locadora(Rotina.gerarFilmes());
 
@@ -17,7 +16,29 @@ public class Principal {
 
 			switch (opcaoMenu) {
 				case "1": {
+					Saida.cabecalhoFuncionalidade("Registrar Filme");
+					
+					Saida.campoDeEntrada("Nome");
+					String nome = Entrada.recebeString();
+
+					Saida.campoDeEntrada("Categoria");
+					String categoria = Entrada.recebeString();
+
+					Saida.campoDeEntrada("Duracao  (Ex: HH:mm)");
+					String duracao = Entrada.recebeString();
+
+					Saida.campoDeEntrada("Preço");
+					double preco = Entrada.recebeDouble();
+
+					Saida.campoDeEntrada("Classificação Indicativa");
+					int classificacao = Entrada.recebeInt();
+			        
+			        locadora.cadastrarFilme(new Filme(nome, categoria, duracao, preco, classificacao));
+					Saida.resultadoFuncao("Cadastro realizado com sucesso!");
+					opcaoMenu = Entrada.recebeString();
+					Saida.limparConsole();
 					break;
+					
 				}
 				case "2": {
 					break;
@@ -37,9 +58,9 @@ public class Principal {
 						Saida.limparConsole();
 					}
 					break;
+					
 				}
 			}
 		} while (!opcaoMenu.equals("0"));
-
 	}
 }
