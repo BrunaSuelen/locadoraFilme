8025734ce490f78147dd098c7175345b71f75abb
Bruna Suelen <subr.garcia@gmail.com>
2022-05-21 23:11:39 -0300

    feat: Melhora o layout do menu e cabeçalhos

--- src/uteis/Saida.java
+++ src/uteis/Saida.java
@@ -1,18 +1,35 @@
 package uteis;
 
+import java.io.IOException;
+
 public class Saida {
 	
 	public static void cabecalho() {
-		System.out.println("\n  :::   LOCADORA DE FILMES   :::");
-		System.out.println("  ==============================\n");
+		System.out.println("\n  :::       LOCADORA DE FILMES       :::");
+		System.out.println("  ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨");
 	}
-
+	
+	public static void cabecalhoFuncionalidade(String funcao) {
+		cabecalho();
+		System.out.println("\t    "+ funcao + "\n");
+	}
+	
 	public static void menu() {
 		cabecalho();
-		System.out.println("  | 1 |  Registrar filmes");
-		System.out.println("  | 2 |  Listar filme(s)");
-		System.out.println("  | 3 |  Sair");
-		System.out.println("\n  ______________________________");
-		System.out.println("  >> Digite a opção desejada: ");
+		System.out.println("\n");
+		System.out.println("  | 1 |  Registrar Filme");
+		System.out.println("  | 2 |  Listar Filme(s)");
+		System.out.println("  | 3 |  Buscar Filme");
+		System.out.println("  | 0 |  Sair");
+		campoDeEntrada("Digite a opção desejada");
+	}
+	
+	
+	public static void campoDeEntrada(String descricaoCampo) {
+		System.out.print("\n  >>  "+ descricaoCampo +": ");
+	}
+	
+	public static void limparConsole() {
+		
 	}
 }
