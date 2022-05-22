6e045afaadf6b94db585d5e368e26287c8542ece
Bruna Suelen <subr.garcia@gmail.com>
2022-05-22 00:42:40 -0300

    feat: Melhora fluxo de resposta da função busca filme

--- src/Principal.java
+++ src/Principal.java
@@ -1,40 +1,45 @@
 import classes.Filme;
 import classes.Locadora;
-
 import java.io.IOException;
 import uteis.*;
 
+
 public class Principal {
 
-	public static void main(String[] args) throws IOException {
-		int opcaoMenu;
+	public static void main(String[] args) throws InterruptedException, IOException {
+		String opcaoMenu;
 		Locadora locadora = new Locadora(Rotina.gerarFilmes());
 
 		do {
 			Saida.menu();
-			opcaoMenu = Entrada.recebeInt();
+			opcaoMenu = Entrada.recebeString();
 			Saida.limparConsole();
 
 			switch (opcaoMenu) {
-				case 1: {
+				case "1": {
 					break;
 				}
-				case 2: {
+				case "2": {
 					break;
 				}
-				case 3: { // Buscar Filme
+				case "3": { // Buscar Filme
 					Saida.cabecalhoFuncionalidade("Buscar Filme");
 					Saida.campoDeEntrada("Informe o nome do filme que deseja");
 					String nomeFilmeDesejado = Entrada.recebeString();
-					Filme filmeEncontrado = locadora.buscarFilme(nomeFilmeDesejado);
-					break;
-				}
-				case 0: {
-					System.exit(0);
+					Filme filme = locadora.buscarFilme(nomeFilmeDesejado);
+					
+					if (filme != null) {
+						Saida.limparConsole();
+						//filme.exibirFilme();
+					} else {
+						Saida.resultadoFuncao("Infelizmente, não encontramos o filme :(");
+						opcaoMenu = Entrada.recebeString();
+						Saida.limparConsole();
+					}
 					break;
 				}
 			}
-		} while (opcaoMenu != 0);
+		} while (!opcaoMenu.equals("0"));
 
 	}
 }
