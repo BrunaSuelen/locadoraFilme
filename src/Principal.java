0783d6045cbe661e86b62c3f9dc6a8405913a57d
Bruna Suelen <subr.garcia@gmail.com>
2022-05-21 23:13:30 -0300

    feat: Inclui a gestão de filmes pela locadora

--- src/Principal.java
+++ src/Principal.java
@@ -1,32 +1,40 @@
-import java.util.Scanner;
 import classes.Filme;
-import java.util.ArrayList;
+import classes.Locadora;
+
+import java.io.IOException;
 import uteis.*;
 
-
 public class Principal {
-	public static void main(String[] args) {
-		ArrayList<Filme> filmes = Rotina.gerarFilmes();
-		Saida.menu();
-		
-		/*
-		 * ArrayList<Filme> locadora = new ArrayList<>(); Scanner scan = new
-		 * Scanner(System.in); int opcao; Filme f;
-		 * 
-		 * do { System.out.println("Sistema de registro de filmes");
-		 * System.out.println("-------------------------------");
-		 * System.out.println("| 1 - Registrar filme        |");
-		 * System.out.println("| 2 - listar filme(s)        |");
-		 * System.out.println("| 3 - Sair                   |");
-		 * System.out.println("-------------------------------");
-		 * System.out.println("Digite sua opção"); opcao = scan.nextInt();
-		 * 
-		 * switch (opcao) {
-		 * 
-		 * case 1: { f = filme.alugarFilme(); locadora.add(f); break; } case 2: {
-		 * filme.exibirLista(locadora); break; } case 3: {
-		 * System.out.println("Processo concluído"); break; } } } while (opcao != 3);
-		 */
+
+	public static void main(String[] args) throws IOException {
+		int opcaoMenu;
+		Locadora locadora = new Locadora(Rotina.gerarFilmes());
+
+		do {
+			Saida.menu();
+			opcaoMenu = Entrada.recebeInt();
+			Saida.limparConsole();
+
+			switch (opcaoMenu) {
+				case 1: {
+					break;
+				}
+				case 2: {
+					break;
+				}
+				case 3: { // Buscar Filme
+					Saida.cabecalhoFuncionalidade("Buscar Filme");
+					Saida.campoDeEntrada("Informe o nome do filme que deseja");
+					String nomeFilmeDesejado = Entrada.recebeString();
+					Filme filmeEncontrado = locadora.buscarFilme(nomeFilmeDesejado);
+					break;
+				}
+				case 0: {
+					System.exit(0);
+					break;
+				}
+			}
+		} while (opcaoMenu != 0);
 
 	}
 }
